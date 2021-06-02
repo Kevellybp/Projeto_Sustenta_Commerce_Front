2e9eeff1ac3f96cfca0e7be1ce4529096717dbf0
Roberto Locatelli <rlocatelli02@gmail.com>
2021-06-02 15:39:35 -0300

    adicionando componentes cadastrar e entrar

---
+++ SustentaCommerce/src/app/model/Categorias.ts
@@ -0,0 +1,7 @@
+import { Produtos } from "./Produtos"
+
+export class Categorias{
+    public idCategoria: number
+    public departamentoProduto: string
+    public categoria_produtos: Produtos[]
+}
---
+++ SustentaCommerce/src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+    public nome: string
+    public usuario: string
+    public senha: string
+    public id: number
+    public token: string
+    public usuarioVendedor: boolean
+    public usuarioAdministrador: boolean
+}
