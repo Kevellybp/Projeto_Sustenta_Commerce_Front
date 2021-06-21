368b523c84e93600681cb9d0ac4a9dcddc61091c
Roberto Locatelli <rlocatelli02@gmail.com>
2021-06-21 11:51:00 -0300

    WIP

--- SustentaCommerce/src/app/model/Categorias.ts
+++
@@ -1,7 +0,0 @@
-import { Produtos } from "./Produtos"
-
-export class Categorias{
-    public id: number
-    public departamentoProduto: string
-    public categoria_produtos: Produtos[]
-}
--- SustentaCommerce/src/app/model/UserLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UserLogin{
-    public nome: string
-    public usuario: string
-    public senha: string
-    public id: number
-    public token: string
-    public usuarioVendedor: boolean
-    public usuarioAdministrador: boolean
-}
